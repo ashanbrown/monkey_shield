b3ec771174e92da293024947bcf641b45ee43180
steve <coderrr.contact@gmail.com>
2009-07-07 23:53:47 +0700

    fixed up bm code

--- spec/benchmarks.rb
+++ spec/benchmarks.rb
@@ -1,20 +1,30 @@
+require 'rubygems'
+
 require 'benchmark'
 require File.dirname(__FILE__) + "/../lib/monkey_shield"
 
+def filename fn, code
+  eval code, nil, fn
+end
+
 MonkeyShield.wrap_with_context :dog do
-  class Animal
-    def speak
-      :bark!
+  filename 'dog.rb', %{
+    class Animal
+      def speak
+        sleep 0.01
+        :bark!
+      end
+
+      def blah
+        sleep 0.01
+        :blah!
+      end
     end
 
-    def blah
-      :blah!
+    def speak_dog!
+      Animal.new.speak
     end
-  end
-
-  def speak_dog!
-    Animal.new.speak
-  end
+  }
 end
 
 def do_blah!
@@ -22,21 +32,24 @@ def do_blah!
 end
 
 MonkeyShield.wrap_with_context :cat do
-  class Animal
-    def speak
-      :meow!
+  filename 'cat.rb', %{
+    class Animal
+      def speak
+        :meow!
+      end
     end
-  end
 
-  def speak_cat!
-    Animal.new.speak
-  end
+    def speak_cat!
+      Animal.new.speak
+    end
+  }
 end
 
 MonkeyShield.context_switch_for Animal, :speak
 
 class Animal
   def something_else
+    sleep 0.01
     :something_else
   end
 end
@@ -45,9 +58,9 @@ def do_something_else
   Animal.new.something_else
 end
 
-t1 = Benchmark.realtime { 100000.times { speak_dog! } }
-t2 = Benchmark.realtime { 100000.times { do_blah! } }
-tp = Benchmark.realtime { 100000.times { do_something_else } }
+t1 = Benchmark.realtime { 100.times { speak_dog! } }
+t2 = Benchmark.realtime { 100.times { do_blah! } }
+tp = Benchmark.realtime { 100.times { do_something_else } }
 
 class Symbol
   def to_proc
@@ -58,7 +71,7 @@ end
 require 'ruby-prof'
 prof = lambda do 
   result = RubyProf.profile do 
-    100000.times { speak_dog! }
+    100.times { speak_dog! }
   end
   printer = RubyProf::FlatPrinter.new(result)
   printer.print(STDOUT, 0)
